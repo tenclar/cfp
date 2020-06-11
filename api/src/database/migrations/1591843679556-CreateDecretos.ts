bbb5b8bdfb4c37a106a0242d5f1f6ebe80c0e1c8
Tenclar <tenclarvalus@gmail.com>
2020-06-11 16:01:42 -0500

    migrations

---
+++ api/src/database/migrations/1591843679556-CreateDecretos.ts
@@ -0,0 +1,50 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateDecreto1591843679556 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'decretos',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'area',
+            type: 'int',
+          },
+          {
+            name: 'pessoasmetro',
+            type: 'int',
+          },
+          {
+            name: 'decreto',
+            type: 'varchar',
+          },
+          {
+            name: 'status',
+            type: 'boolean',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('decretos');
+  }
+}
---
+++ api/src/database/migrations/1591846378205-CreateAtendimentos.ts
@@ -0,0 +1,67 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateAtendimento1591846378205
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'atendimentos',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'decreto_id',
+            type: 'uuid',
+          },
+
+          {
+            name: 'valor',
+            type: 'int',
+          },
+          {
+            name: 'tipo',
+            type: 'int',
+          },
+
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'atendimentos',
+      new TableForeignKey({
+        name: 'AtendimentoDecreto',
+        columnNames: ['decreto_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'decretos',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('atendimentos', 'AtendimentoDecreto');
+    await queryRunner.dropTable('atendimentos');
+  }
+}
