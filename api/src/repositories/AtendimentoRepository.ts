dd8c3692eda8a1937dfad3d80aedd869819ebe90
Tenclar <tenclarvalus@gmail.com>
2020-06-11 16:06:25 -0500

    repositories

---
+++ api/src/repositories/AtendimentoRepository.ts
@@ -0,0 +1,9 @@
+/* import { EntityRepository, Repository } from 'typeorm';
+import Decreto from '../models/Decreto';
+
+@EntityRepository(Decreto)
+
+class DecretoRepository extends Repository<Decreto> {}
+
+export default DecretoRepository;
+ */
